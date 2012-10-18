fa142bf2ca525a7f85a0c5ccda135e503566b655
Simon Constans <kosssi@gmail.com>
2012-10-18 23:05:40 +0200

    Init

---
+++ src/kosssi/PreBundle/EventListener/HelloListener.php
@@ -0,0 +1,19 @@
+<?php
+namespace kosssi\PreBundle\EventListener;
+
+use Whisnet\IrcBotBundle\EventListener\Plugins\Commands\CommandListener;
+use Whisnet\IrcBotBundle\Event\BotCommandFoundEvent;
+
+class HelloListener extends CommandListener
+{
+    public function onCommand(BotCommandFoundEvent $event)
+    {
+        // get list of arguments passed after command
+        //$args = $event->getArguments();
+
+        //$msg = 'Hi, '.(isset($args[0]) ? $args[0] : 'nobody').' !';
+
+        // write to the current channel
+        //$this->sendMessage(array($event->getChannel()), $msg);
+    }
+}
---
+++ src/kosssi/PreBundle/PreBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace kosssi\PreBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class PreBundle extends Bundle
+{
+}
