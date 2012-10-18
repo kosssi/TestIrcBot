6629b0b26b0db43075a414814f738fda1444d1e9
Simon Constans <kosssi@gmail.com>
2012-10-19 00:50:47 +0200

    fix service

--- src/kosssi/PreBundle/EventListener/HelloListener.php
+++ src/kosssi/PreBundle/EventListener/HelloListener.php
@@ -9,11 +9,11 @@ class HelloListener extends CommandListener
     public function onCommand(BotCommandFoundEvent $event)
     {
         // get list of arguments passed after command
-        //$args = $event->getArguments();
+        $args = $event->getArguments();
 
-        //$msg = 'Hi, '.(isset($args[0]) ? $args[0] : 'nobody').' !';
+        $msg = 'Hi, '.(isset($args[0]) ? $args[0] : 'nobody').' !';
 
         // write to the current channel
-        //$this->sendMessage(array($event->getChannel()), $msg);
+        $this->sendMessage(array($event->getChannel()), $msg);
     }
 }
